7404afb1a7a5aaa169d68de9dbb693e7ebc445d5
Sanjoy Biswas <40058484+imsanjoykb@users.noreply.github.com>
2020-11-14 23:21:21 +0600

    Update Exercise-24-Draw-Game-Board.py

--- Exercise-24-Draw-Game-Board.py
+++ Exercise-24-Draw-Game-Board.py
@@ -18,7 +18,7 @@ This one is 3x3 (like in tic tac toe). Obviously, they come in many
 other sizes (8x8 for chess, 19x19 for Go, and many more).
 
 Ask the user what size game board they want to draw, and draw it
-for them to the screen using Python’s print statement.
+for them to the screen using Python’s print statement...
 
 '''
 
