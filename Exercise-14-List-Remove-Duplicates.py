5d1650436125b6a60bdef3d99e696487542b5113
Sanjoy Biswas <40058484+imsanjoykb@users.noreply.github.com>
2020-11-18 21:54:10 +0600

    Update Exercise-14-List-Remove-Duplicates.py

--- Exercise-14-List-Remove-Duplicates.py
+++ Exercise-14-List-Remove-Duplicates.py
@@ -2,7 +2,7 @@
 Exercise 14: List Remove Duplicates
 Write a program (function!) that takes a list and returns a new
 list that contains all the elements of the first list minus all
-the duplicates.
+the duplicates values.
 
 Extras:
 
