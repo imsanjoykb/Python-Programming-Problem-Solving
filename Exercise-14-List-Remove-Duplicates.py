81003846acf9ec2d49eef0c9512efa1f9e9e802e
Sanjoy Biswas <40058484+imsanjoykb@users.noreply.github.com>
2020-11-18 21:54:34 +0600

    Update Exercise-14-List-Remove-Duplicates.py

--- Exercise-14-List-Remove-Duplicates.py
+++ Exercise-14-List-Remove-Duplicates.py
@@ -9,7 +9,7 @@ Extras::
 Write two different functions to do this - one using a loop and
 constructing a list, and another using sets.
 Go back and do Exercise 5 using sets, and write the solution for
-that in a different function.
+that in a different function...
 
 '''
 
