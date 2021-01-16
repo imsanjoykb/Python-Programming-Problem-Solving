f08cbd2f9244186888a13f79254d1760774564ed
Sanjoy Biswas <40058484+imsanjoykb@users.noreply.github.com>
2021-01-16 22:57:53 +0600

    Update Exercise-20-Element-Search.py

--- Exercise-20-Element-Search.py
+++ Exercise-20-Element-Search.py
@@ -5,7 +5,7 @@ Write a function that takes an ordered list of numbers (a list
 where the elements are in order from smallest to largest) and
 another number. The function decides whether or not the given
 number is inside the list and returns (then prints) an
-appropriate boolean......
+appropriate boolean......-----------------
 
 Extras:::
 Use binary search..
