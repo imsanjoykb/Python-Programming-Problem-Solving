d2bcfc543cb415c35a250d6a889093916a7153c5
Sanjoy Biswas <40058484+imsanjoykb@users.noreply.github.com>
2021-02-27 23:19:21 +0600

    Update Exercise-03-List-Less-Than-Ten.py

--- Exercise-03-List-Less-Than-Ten.py
+++ Exercise-03-List-Less-Than-Ten.py
@@ -8,11 +8,7 @@ and write a program that prints out all the elements of the list that are less t
 
 Extras:
 
-1. Instead of printing the elements one by one, make a new list that has all the elements
-less than 5 from this list in it and print out this new list.
-2. Write this in one line of Python.
-3. Ask the user for a number and return a list that contains only elements from the
-original list a that are smaller than that number given by the user.
+the user.
 
 '''
 
