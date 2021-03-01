8f4066ce514fad6c223c1e7779dcd14be9737e89
Sanjoy Biswas <40058484+imsanjoykb@users.noreply.github.com>
2021-03-01 23:51:02 +0600

    Update Exercise-22-Read-From-a-File.py

--- Exercise-22-Read-From-a-File.py
+++ Exercise-22-Read-From-a-File.py
@@ -1,10 +1,6 @@
 '''
 Exercise 22: Read From File
-
-Given a .txt file that has a list of a bunch of names, count
-how many of each name there are in the file, and print out
-the results to the screen. I have a .txt file for you, if
-you want to use it!
+''''
 
 Extra:
 Instead of using the .txt file from above (or instead of, if
